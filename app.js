e7f974fa112044f9808b66df129fa97fdc60d5e3
ajdrew <ajdrew@outlook.com>
2015-10-23 10:14:07 -0400

    s

--- app.js
+++ app.js
@@ -356,7 +356,7 @@ app.get('/extensions/filter/mx', function(req, res){
 
 app.post('/extensions-find', function(req, res){
     console.log(req.body);
-    var search = { req.body.search };
+    var search = 'req.body.search';
     //var search = 'MX';
     var listData = function(err, collection) {
         collection.find({place: new RegExp(search)}).toArray(function(err, results) {

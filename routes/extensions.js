fca9c30407e8a987dce6160ba4f9b9fa4edd3152
ajdrew <ajdrew@outlook.com>
2015-10-26 16:24:18 -0400

    search t

--- routes/extensions.js
+++ routes/extensions.js
@@ -222,7 +222,7 @@ module.exports = function(app) {
       //console.log(echo $search);
       //var search = 'MX';
       var listData = function(err, collection) {
-          collection.find({whole_name: new RegExp(search)}).toArray(function(err, results) {
+          collection.find({$or:[{extension: new RegExp(search)},{whole_name: new RegExp(search)}).toArray(function(err, results) {
               res.render('extensions-show.html', { layout : false , 'title' : 'Amway.voice', 'results' : results });
           });
       }

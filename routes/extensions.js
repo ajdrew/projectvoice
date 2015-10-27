9e6b91e60f170df03d17e2748ae0f201ff6a48fd
ajdrew <ajdrew@outlook.com>
2015-10-27 14:20:51 -0400

    t

--- routes/extensions.js
+++ routes/extensions.js
@@ -40,7 +40,7 @@ module.exports = function(app) {
   // DB - CRUD
   app.post('/extensions/save', function(req, res){
       console.log(req.body);
-      var data = {'extension' : req.body.extension , 'user_id' : req.body.user_id, 'whole_name' : req.body.whole_name, 'email' : req.body.email, 'place' : req.body.place, 'phone_number_jabber' : req.body.phone_number_jabber, 'did' : req.body.did, 'out_date' : req.body.ot_date };
+      var data = {'extension' : req.body.extension , 'user_id' : req.body.user_id, 'whole_name' : req.body.whole_name, 'email' : req.body.email, 'place' : req.body.place, 'phone_number_jabber' : req.body.phone_number_jabber, 'did' : req.body.did, 'out_date' : req.body.out_date };
       var insertData = function(err, collection) {
           collection.insert(data);
       }

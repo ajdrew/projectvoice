ee94a738d1b60ff18660359cb3c0f3d8b4e41502
ajdrew <ajdrew@outlook.com>
2015-10-28 17:07:20 -0400

    admin update

--- routes/lms-admin.js
+++ routes/lms-admin.js
@@ -37,46 +37,6 @@ module.exports = function(app) {
       res.redirect('/lms/admin');
   });
 
-  app.get('/lms/admin/type/edit/:id', function(req, res){
-
-      var ObjectID = require('mongodb').ObjectID;
-
-      var listData = function(err, collection) {
-
-          var chosenId = new ObjectID(req.params.id);
-          collection.findOne({'_id' : chosenId} , function(err, results) {
-              console.log(results);
-              res.render('extensions-edit.html', { layout : false , 'title' : 'Amway.voice', 'results' : results });
-          });
-      }
-
-      var Client = new Db('amway-voice', new Server('172.30.53.200', 27017, {}));
-      Client.open(function(err, pClient) {
-          Client.collection('lmsadmintype', listData);
-          //Client.close();
-      });
-
-  });
-
-  app.post('/lms/admin/type/update', function(req, res){
-      console.log(req.body);
-
-      var ObjectID = require('mongodb').ObjectID;
-
-      var data = {'extension' : req.body.extension , 'user_id' : req.body.user_id, 'whole_name' : req.body.whole_name, 'email' : req.body.email, 'place' : req.body.place, 'phone_number_jabber' : req.body.phone_number_jabber, 'did' : req.body.did };
-      var updateData = function(err, collection) {
-          var chosenId = new ObjectID(req.body.id);
-          collection.update({"_id": chosenId}, {$set: data });
-      }
-      var Client = new Db('amway-voice', new Server('172.30.53.200', 27017, {}));
-      Client.open(function(err, pClient) {
-          Client.collection('lmsadmintype', updateData);
-          Client.close();
-      });
-
-      res.redirect('/lsm/admin');
-  });
-
   app.get('/lms/admin/type/delete/:id', function(req, res){
       var ObjectID = require('mongodb').ObjectID;
 

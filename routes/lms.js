792a571fd7f81182532aaf81bbebf9ab1ab46a96
ajdrew <ajdrew@outlook.com>
2015-11-16 16:32:14 -0500

    t

--- routes/lms.js
+++ routes/lms.js
@@ -181,7 +181,7 @@ module.exports = function(app) {
     }
     var Client = new Db('amway-voice', new Server('172.30.53.200', 27017, {}));
     Client.open(function(err, pClient) {
-      Client.collection('extensions', insertData);
+      Client.collection('lms', insertData);
       Client.close();
     });
 

c0c1821699b2826a3cd08528a2096235f098e41c
ajdrew <ajdrew@outlook.com>
2015-12-04 14:12:34 -0500

    t

--- routes/extensions-admin.js
+++ routes/extensions-admin.js
@@ -23,7 +23,7 @@ module.exports = function(app) {
 
     var Client = new Db('amway-voice', new Server('172.30.53.200', 27017, {}));
     Client.open(function(err, pClient) {
-      Client.collection('extensionsadminlocations', listDataType);
+      Client.collection('extensionsadminlocations', listDataLocations);
     });
 
     function complete() {

ce330bf0e3f9dfcd380e32dfff485c4cec1fcd1d
ajdrew <ajdrew@outlook.com>
2015-12-17 17:12:06 -0500

    e

--- routes/lms.js
+++ routes/lms.js
@@ -211,7 +211,7 @@ module.exports = function(app) {
         '_id': chosenId
       }, function(err, results) {
         console.log(results);
-        res.render('lms-edit.html', {
+        res.render('lms/lms-edit.html', {
           layout: false,
           'title': 'Amway.voice',
           'results': results

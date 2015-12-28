3a31eb604c69c622f64df061dfa85667ee7f69a4
ajdrew <ajdrew@outlook.com>
2015-12-28 11:06:20 -0500

    Authorization re-write

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-// Module dependencies
+// MODULES - EXPRESS
 var express = require('express'),
   routes = require('./routes'),
   http = require('http');
-
-// template engine
+// MODULES - TEMPLATE ENGINE
 var hbs = require('hbs');
 
-// mongodb
+// MODULES - MONGODB
 var Db = require('mongodb').Db;
 var Server = require('mongodb').Server;
 
+// CONFIGURATION - EXPRESS SETUP
 var app = express();
-
 app.configure(function() {
   app.set('port', process.env.PORT || 80);
-
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -26,23 +24,19 @@ app.configure(function() {
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
-
   app.engine('html', require('hbs').__express);
   app.set('views', __dirname + '/views/html');
   app.set('view engine', 'html');
-
-  // app.set('views', __dirname + '/views');
-  // app.set('view engine', 'jade');
-
 });
 
+// CONFIGURATION - ERROR HANDELING OUTPUT
 app.configure('development', function() {
   app.use(express.errorHandler());
 });
 
+// AUTHORIZATION - JS AUTHORIZATION CHECK
 function checkAuth(req, res, next) {
   console.log('checkAuth ' + req.url);
-
   // don't serve /secure to those not logged in
   // you should add to this list, for each and every secure url
   if (req.url === '/lms' && (!req.session || !req.session.authenticated)) {
@@ -56,9 +50,7 @@ function checkAuth(req, res, next) {
   next();
 }
 
-
-// EXPRESS ROUTING
-// NAVIGATION - INDEX
+// EXPRESS ROUTING - INDEX
 app.get('/', function(req, res) {
   res.render('index.html', {
     layout: false,
@@ -66,24 +58,21 @@ app.get('/', function(req, res) {
   });
 })
 
-
-// INCLUDE - Extensions DB app routes
+// EXPRESS ROUTING - INCLUDE - Extensions DB app routes
 var routingextensions = require('./routes/extensions.js')(app);
 // INCLUDE - Extensions-admin DB app routes
 var routingextensions = require('./routes/extensions-admin.js')(app);
 
-// INCLUDE - LMS DB app routes
+// EXPRESS ROUTING - INCLUDE - LMS DB app routes
 var routingextensions = require('./routes/lms.js')(app);
-// INCLUDE - LMS-admin DB app routes
+// EXPRESS ROUTING - INCLUDE - LMS-admin DB app routes
 var routingextensions = require('./routes/lms-admin.js')(app);
 
-// INCLUDE - Authorization app routes
+// EXPRESS ROUTING - INCLUDE - Authorization app routes
 var routingextensions = require('./routes/authorization.js')(app);
 
 
-
-// END - EXPRESS ROUTING
-
+// SERVER - SETUP AND RUN
 http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 });
